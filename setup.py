1c780df57b1c1e2408bf31a52b64576210e7d7d7
Mikhail Advani <mikhail.advani@shell.com>
2022-05-03 17:06:34 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+    install_requires = []
+    for dependency in requirements:
+      if dependency.startswith("git+"):
+        package_name = dependency.split("/")[-1].split("@")[0]
+        dependency = package_name + ' @ ' + dependency
+        print(dependency)
+      install_requires.append(dependency)
+
+setuptools.setup(
+    name="pip_package",
+    version="0.0.1",
+    author=author,
+    url="https://github.com/mikhailadvani/pip_package",
+    install_requires=install_requires
+)
